d544233a99d741e9a487467e28d351beded0e831
Ivan <ivanszhuravlev@gmail.com>
2019-05-28 23:45:54 +0300

    Add heading

---
+++ src/components/Heading/HeadingStyles.js
@@ -0,0 +1,11 @@
+import {StyleSheet} from 'react-native'
+
+const styles = StyleSheet.create({
+    heading: {
+        fontSize: 24,
+        lineHeight: 32,
+        fontWeight: '600'
+    },
+});
+
+export default styles
--- src/pages/UserForm/UserForm.js
+++ src/pages/UserForm/UserForm.js
@@ -2,11 +2,16 @@ import React, { Component } from 'react'
 import { Text, View, ScrollView} from 'react-native'
 import Input from '../../components/Input/Input';
 import styles from './UserFormStyles'
+import StatusBar from '../../components/StatusBar/StatusBar'
+import Heading from '../../components/Heading/Heading'
 
 export class UserForm extends Component {
     render() {
         return (
             <ScrollView style={styles.wrapper}>
+                <Heading>
+                    Edit profile
+                </Heading>
                 <Input 
                     placeholder="First name"
                 />
