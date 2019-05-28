3b1f9cf2475282f8da049b42f322fbfc762f8111
Ivan <ivanszhuravlev@gmail.com>
2019-05-28 23:45:25 +0300

    Add statusbar

--- App.js
+++ App.js
@@ -1,16 +1,18 @@
 import React, {Component} from 'react';
 import {Platform, StyleSheet, Text, View} from 'react-native';
 import UserForm from './src/pages/UserForm/UserForm';
+import StatusBar from './src/components/StatusBar/StatusBar'
 
 const styles = StyleSheet.create({
   app: {
-    paddingTop: 24
+    paddingTop: 21
   }
 })
 export default class App extends Component<Props> {
   render() {
     return (
       <View style={styles.app}>
+        <StatusBar />
         <UserForm />
       </View>
     );
